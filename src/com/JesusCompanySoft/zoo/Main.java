684ee925f3f054dea798beffe1d1297814b5c0bc
jesusMelian <usr2685@salesianos-lacuesta.net>
2021-10-01 14:53:04 +0100

    Features: aniadida la función para generar un número aleatorio

--- src/com/JesusCompanySoft/zoo/Main.java
+++ src/com/JesusCompanySoft/zoo/Main.java
@@ -1,15 +1,17 @@
 package com.JesusCompanySoft.zoo;
 
+import com.JesusCompanySoft.zoo.Animals.Animals;
+
 import javax.swing.JOptionPane;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
         int espacios = Integer.parseInt(JOptionPane.showInputDialog("INSERTE EL NÚMERO DE ESPACIOS"));
         int animales = Integer.parseInt(JOptionPane.showInputDialog("INSERTE EL NÚMERO DE ANIMALES"));
 
-
-
         if(comprobarSiCaben(espacios, animales)){
+            //si los animales caben hay que generar animales aleatoriamente
 
         }else{
             System.out.println("LOS ANIMALES NO CABEN BIEN");
@@ -24,4 +26,15 @@ public class Main {
             return true;
         }
     }
+
+    public static double generarNumeroAleatorio(double numMin, double numMax){
+        double randomNum = Math.random() * ((numMax+1) - numMin);
+        return  randomNum;
+    }
+
+    public static ArrayList<Animals> generarAnimales(int espacios, int animales){
+        ArrayList<Animals> animal = new ArrayList<>();
+        return animal;
+    }
+
 }
